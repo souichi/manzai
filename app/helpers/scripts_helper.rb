87cd51beea96e94214dc54509e155b73dc8402dd
Soichi Saito <souichi.saitou@gmail.com>
2015-09-28 03:49:21 +0900

    to icon

--- app/helpers/scripts_helper.rb
+++ app/helpers/scripts_helper.rb
@@ -1,2 +1,14 @@
 module ScriptsHelper
+  def icon_class(target)
+    case target
+    when "whole"
+      return "fa fa-users"
+    when "boke"
+      return "fa fa-male"
+    when "tsukkomi"
+      return "fa fa-female"
+    when "message"
+      return "fa fa-commenting"
+    end
+  end
 end
