d47bba042b281a064728869df23286a411232f40
齋藤宗市 <s.saito@gloops.com>
2015-09-30 11:49:56 +0900

    for turbolinks

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -1,19 +1,21 @@
 define(["require", "exports", "./manzai"], function (require, exports, manzai) {
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", location.href + ".json");
-    xhr.addEventListener("loadend", function () {
-        var script = JSON.parse(xhr.responseText);
-        var timeline = new manzai.Timeline(script);
-        document.addEventListener("click", onclick);
-        function onclick() {
-            if (timeline.playing) {
-                timeline.play();
+    document.addEventListener("page:change", function () {
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", location.href + ".json");
+        xhr.addEventListener("loadend", function () {
+            var script = JSON.parse(xhr.responseText);
+            var timeline = new manzai.Timeline(script);
+            document.addEventListener("click", onclick);
+            function onclick() {
+                if (timeline.playing) {
+                    timeline.play();
+                }
+                else {
+                    timeline.end();
+                    document.removeEventListener("click", onclick);
+                }
             }
-            else {
-                timeline.end();
-                document.removeEventListener("click", onclick);
-            }
-        }
+        });
+        xhr.send();
     });
-    xhr.send();
 });
--- app/assets/javascripts/scripts.ts
+++ app/assets/javascripts/scripts.ts
@@ -1,19 +1,21 @@
 import * as manzai from "./manzai";
 
-var xhr = new XMLHttpRequest();
-xhr.open("GET", location.href + ".json");
-xhr.addEventListener("loadend", () => {
-  var script = <manzai.IScript> JSON.parse(xhr.responseText);
-  var timeline = new manzai.Timeline(script);
-  document.addEventListener("click", onclick);
-  
-  function onclick(): void {
-    if (timeline.playing) {
-      timeline.play();
-    } else {
-      timeline.end();
-      document.removeEventListener("click", onclick);
+document.addEventListener("page:change", () =>{
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", location.href + ".json");
+  xhr.addEventListener("loadend", () => {
+    var script = <manzai.IScript> JSON.parse(xhr.responseText);
+    var timeline = new manzai.Timeline(script);
+    document.addEventListener("click", onclick);
+    
+    function onclick(): void {
+      if (timeline.playing) {
+        timeline.play();
+      } else {
+        timeline.end();
+        document.removeEventListener("click", onclick);
+      }
     }
-  }
+  });
+  xhr.send();
 });
-xhr.send();
