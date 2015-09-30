ae3d60fe8c16b1853e8f2333992b4319e98c79be
齋藤宗市 <s.saito@gloops.com>
2015-09-30 12:25:57 +0900

    turbolinks off / load async enchant

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -1,21 +1,33 @@
 define(["require", "exports", "./manzai"], function (require, exports, manzai) {
-    document.addEventListener("page:change", function () {
-        var xhr = new XMLHttpRequest();
-        xhr.open("GET", location.href + ".json");
-        xhr.addEventListener("loadend", function () {
-            var script = JSON.parse(xhr.responseText);
-            var timeline = new manzai.Timeline(script);
-            document.addEventListener("click", onclick);
-            function onclick() {
-                if (timeline.playing) {
-                    timeline.play();
-                }
-                else {
-                    timeline.end();
-                    document.removeEventListener("click", onclick);
-                }
+    loadEnchantJs();
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", location.href + ".json");
+    xhr.addEventListener("loadend", function () {
+        var script = JSON.parse(xhr.responseText);
+        var timeline = new manzai.Timeline(script);
+        document.addEventListener("click", onclick);
+        function onclick() {
+            if (timeline.playing) {
+                timeline.play();
             }
-        });
-        xhr.send();
+            else {
+                timeline.end();
+                document.removeEventListener("click", onclick);
+            }
+        }
     });
+    xhr.send();
+    function loadEnchantJs() {
+        (function () {
+            var script = document.createElement('script');
+            script.type = 'text/javascript';
+            script.async = true;
+            script.src = 'https://cdn.jsdelivr.net/enchantjs/0.8.1/enchant.min.js';
+            document.getElementsByTagName('script')[0].parentNode.appendChild(script);
+            var viewport = document.createElement("meta");
+            viewport.name = "viewport";
+            viewport.content = "width=320, initial-scale=1.0, user-scalable=no";
+            document.getElementsByTagName('script')[0].parentNode.appendChild(viewport);
+        })();
+    }
 });
--- app/assets/javascripts/scripts.ts
+++ app/assets/javascripts/scripts.ts
@@ -1,21 +1,35 @@
 import * as manzai from "./manzai";
 
-document.addEventListener("page:change", () =>{
-  var xhr = new XMLHttpRequest();
-  xhr.open("GET", location.href + ".json");
-  xhr.addEventListener("loadend", () => {
-    var script = <manzai.IScript> JSON.parse(xhr.responseText);
-    var timeline = new manzai.Timeline(script);
-    document.addEventListener("click", onclick);
-    
-    function onclick(): void {
-      if (timeline.playing) {
-        timeline.play();
-      } else {
-        timeline.end();
-        document.removeEventListener("click", onclick);
-      }
+loadEnchantJs();
+
+var xhr = new XMLHttpRequest();
+xhr.open("GET", location.href + ".json");
+xhr.addEventListener("loadend", () => {
+  var script = <manzai.IScript> JSON.parse(xhr.responseText);
+  var timeline = new manzai.Timeline(script);
+  document.addEventListener("click", onclick);
+  
+  function onclick(): void {
+    if (timeline.playing) {
+      timeline.play();
+    } else {
+      timeline.end();
+      document.removeEventListener("click", onclick);
     }
-  });
-  xhr.send();
+  }
 });
+xhr.send();
+
+function loadEnchantJs(): void {
+  (() => {
+      var script = document.createElement('script');
+      script.type = 'text/javascript';
+      script.async = true;
+      script.src = 'https://cdn.jsdelivr.net/enchantjs/0.8.1/enchant.min.js';
+      document.getElementsByTagName('script')[0].parentNode.appendChild(script);
+      // var viewport = document.createElement("meta");
+      // viewport.name = "viewport";
+      // viewport.content = "width=320, initial-scale=1.0, user-scalable=no";
+      // document.getElementsByTagName('script')[0].parentNode.appendChild(viewport);
+  })();
+}
